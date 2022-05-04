1105c7b4376970280aaadfc26f13b41346f5d2bd
MichaelWu <vmichael.wu@gmail.com>
2022-05-03 18:19:31 -0700

    cleaned up code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-const data = require('../data');
+var data = require('../data');
 
 app.listen(3000, () => {
   console.log('Server running on port http://localhost:3000');
@@ -22,9 +22,9 @@ app.get('/', (req, res) => {
 
 app.get('/recipes', (req, res) => {
   let result = [];
-  for (var x = 0; x < data.recipes.length; x++) {
-    result.push(data.recipes[x].name);
-  }
+
+  data.recipes.forEach((recipe) => result.push(recipe.name));
+
   res.status(200).send({
     recipeNames: result,
   });
@@ -32,64 +32,99 @@ app.get('/recipes', (req, res) => {
 
 app.get('/recipes/details/:name', (req, res) => {
   let results = {};
-  let recipeName = req.params.name;
+  let new_recipe_name = req.params.name;
 
-  for (var x = 0; x < data.recipes.length; x++) {
-    if (data.recipes[x].name === recipeName) {
-      results.ingredients = data.recipes[x].ingredients;
-      results.numSteps = data.recipes[x].instructions.length;
-    }
-  }
+  // for (var x = 0; x < data.recipes.length; x++) {
+  //   if (data.recipes[x].name === recipeName) {
+  //     results.ingredients = data.recipes[x].ingredients;
+  //     results.numSteps = data.recipes[x].instructions.length;
+  //   }
+  // }
 
-  if (results.ingredients === undefined) {
-    res.status(200).send(results);
-  } else {
-    res.status(200).send({
-      details: results,
-    });
-  }
+  // if (results.ingredients === undefined) {
+  //   res.status(200).send(results);
+  // } else {
+  //   res.status(200).send({
+  //     details: results,
+  //   });
+  // }
+
+  data.recipes.forEach((recipe) =>
+    recipe.name === new_recipe_name
+      ? ((results.ingredients = recipe.ingredients),
+        (results.numSteps = recipe.instructions.length))
+      : ''
+  );
+
+  results.ingredients === undefined
+    ? res.status(200).send(results)
+    : res.status(200).send({ details: results });
 });
 
 app.post('/recipes', (req, res) => {
   let new_recipe = req.body;
   let new_recipe_name = req.body.name;
   let recipe_exists = false;
+  let error_string = 'Recipe already exists';
 
-  for (var x = 0; x < data.recipes.length; x++) {
-    let recipeName = data.recipes[x].name;
-    if (recipeName === new_recipe_name) {
-      recipe_exists = true;
-    }
-  }
+  // for (var x = 0; x < data.recipes.length; x++) {
+  //   let recipeName = data.recipes[x].name;
+  //   if (recipeName === new_recipe_name) {
+  //     recipe_exists = true;
+  //   }
+  // }
 
-  if (recipe_exists) {
-    res.status(400).send({
-      error: 'Recipe already exists',
-    });
-  } else {
-    data.recipes.push(new_recipe);
-    res.status(201).send();
-  }
+  data.recipes.forEach((recipe) =>
+    recipe.name === new_recipe_name ? (recipe_exists = true) : ''
+  );
+
+  recipe_exists
+    ? res.status(400).send({ error: error_string })
+    : (data.recipes.push(new_recipe), res.status(201).send());
+
+  // if (recipe_exists) {
+  //   res.status(400).send({
+  //     error: 'Recipe already exists',
+  //   });
+  // } else {
+  //   data.recipes.push(new_recipe);
+  //   res.status(201).send();
+  // }
 });
 
 app.put('/recipes', (req, res) => {
   let new_recipe = req.body;
   let new_recipe_name = req.body.name;
   let recipe_exists = false;
+  let error_string = 'Recipe does not exist';
 
-  for (var x = 0; x < data.recipes.length; x++) {
-    let recipeName = data.recipes[x].name;
-    if (recipeName === new_recipe_name) {
-      recipe_exists = true;
-      data.recipes[x] = new_recipe;
-    }
-  }
+  // for (var x = 0; x < data.recipes.length; x++) {
+  //   let recipeName = data.recipes[x].name;
+  //   if (recipeName === new_recipe_name) {
+  //     recipe_exists = true;
+  //     data.recipes[x] = new_recipe;
+  //   }
+  // }
 
-  if (!recipe_exists) {
-    res.status(404).send({
-      error: 'Recipe does not exist',
-    });
-  } else {
-    res.status(204).send();
-  }
+  // data.recipes.forEach((recipe) =>
+  //   recipe.name === new_recipe_name
+  //     ? ((recipe_exists = true), (recipe = new_recipe))
+  //     : ''
+  // );
+
+  data = data.recipes.map((recipe) =>
+    recipe.name === new_recipe_name
+      ? ((recipe = new_recipe), (recipe_exists = true))
+      : ''
+  );
+
+  recipe_exists ? res.status(204).send() : res.status(404).send(error_string);
+
+  // if (!recipe_exists) {
+  //   res.status(404).send({
+  //     error: 'Recipe does not exist',
+  //   });
+  // } else {
+  //   res.status(204).send();
+  // }
 });
