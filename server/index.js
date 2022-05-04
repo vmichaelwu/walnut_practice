301f3afda5ea15ef3f59f1b232ba25f0353659e0
MichaelWu <vmichael.wu@gmail.com>
2022-05-03 19:35:37 -0700

    completed with fs.writefile and fs.readfile

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 var data = require('../data');
+const fs = require('fs');
 
 app.listen(3000, () => {
   console.log('Server running on port http://localhost:3000');
@@ -16,17 +17,15 @@ const myLogger = function (req, res, next) {
 app.use(myLogger);
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send(data);
-});
-
 app.get('/recipes', (req, res) => {
   let result = [];
 
-  data.recipes.forEach((recipe) => result.push(recipe.name));
-
-  res.status(200).send({
-    recipeNames: result,
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    let parsedData = JSON.parse(data);
+    parsedData.recipes.forEach((recipe) => result.push(recipe.name));
+    res.status(200).send({
+      recipeNames: result,
+    });
   });
 });
 
@@ -34,31 +33,20 @@ app.get('/recipes/details/:name', (req, res) => {
   let results = {};
   let new_recipe_name = req.params.name;
 
-  // for (var x = 0; x < data.recipes.length; x++) {
-  //   if (data.recipes[x].name === recipeName) {
-  //     results.ingredients = data.recipes[x].ingredients;
-  //     results.numSteps = data.recipes[x].instructions.length;
-  //   }
-  // }
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    let parsedData = JSON.parse(data);
 
-  // if (results.ingredients === undefined) {
-  //   res.status(200).send(results);
-  // } else {
-  //   res.status(200).send({
-  //     details: results,
-  //   });
-  // }
+    parsedData.recipes.forEach((recipe) =>
+      recipe.name === new_recipe_name
+        ? ((results.ingredients = recipe.ingredients),
+          (results.numSteps = recipe.instructions.length))
+        : ''
+    );
 
-  data.recipes.forEach((recipe) =>
-    recipe.name === new_recipe_name
-      ? ((results.ingredients = recipe.ingredients),
-        (results.numSteps = recipe.instructions.length))
-      : ''
-  );
-
-  results.ingredients === undefined
-    ? res.status(200).send(results)
-    : res.status(200).send({ details: results });
+    results.ingredients === undefined
+      ? res.status(200).send(results)
+      : res.status(200).send({ details: results });
+  });
 });
 
 app.post('/recipes', (req, res) => {
@@ -67,29 +55,26 @@ app.post('/recipes', (req, res) => {
   let recipe_exists = false;
   let error_string = 'Recipe already exists';
 
-  // for (var x = 0; x < data.recipes.length; x++) {
-  //   let recipeName = data.recipes[x].name;
-  //   if (recipeName === new_recipe_name) {
-  //     recipe_exists = true;
-  //   }
-  // }
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    let parsedData = JSON.parse(data);
 
-  data.recipes.forEach((recipe) =>
-    recipe.name === new_recipe_name ? (recipe_exists = true) : ''
-  );
+    parsedData.recipes.forEach((recipe) =>
+      recipe.name === new_recipe_name ? (recipe_exists = true) : ''
+    );
 
-  recipe_exists
-    ? res.status(400).send({ error: error_string })
-    : (data.recipes.push(new_recipe), res.status(201).send());
-
-  // if (recipe_exists) {
-  //   res.status(400).send({
-  //     error: 'Recipe already exists',
-  //   });
-  // } else {
-  //   data.recipes.push(new_recipe);
-  //   res.status(201).send();
-  // }
+    if (recipe_exists) {
+      res.status(400).send({ error: error_string });
+    } else {
+      parsedData.recipes.push(new_recipe);
+      fs.writeFile('data.json', JSON.stringify(parsedData), (err) => {
+        if (err) {
+          throw err;
+        } else {
+          res.status(201).send();
+        }
+      });
+    }
+  });
 });
 
 app.put('/recipes', (req, res) => {
@@ -98,33 +83,31 @@ app.put('/recipes', (req, res) => {
   let recipe_exists = false;
   let error_string = 'Recipe does not exist';
 
-  // for (var x = 0; x < data.recipes.length; x++) {
-  //   let recipeName = data.recipes[x].name;
-  //   if (recipeName === new_recipe_name) {
-  //     recipe_exists = true;
-  //     data.recipes[x] = new_recipe;
-  //   }
-  // }
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    let parsedData = JSON.parse(data);
 
-  // data.recipes.forEach((recipe) =>
-  //   recipe.name === new_recipe_name
-  //     ? ((recipe_exists = true), (recipe = new_recipe))
-  //     : ''
-  // );
+    parsedData.recipes.forEach((recipe) =>
+      recipe.name === new_recipe_name ? (recipe_exists = true) : ''
+    );
 
-  data = data.recipes.map((recipe) =>
-    recipe.name === new_recipe_name
-      ? ((recipe = new_recipe), (recipe_exists = true))
-      : ''
-  );
+    let modified_recipe = parsedData.recipes.map((recipe) =>
+      recipe.name === new_recipe_name ? (recipe = new_recipe) : recipe
+    );
 
-  recipe_exists ? res.status(204).send() : res.status(404).send(error_string);
+    modified_recipe = {
+      recipes: modified_recipe,
+    };
 
-  // if (!recipe_exists) {
-  //   res.status(404).send({
-  //     error: 'Recipe does not exist',
-  //   });
-  // } else {
-  //   res.status(204).send();
-  // }
+    const writeFile = () => {
+      fs.writeFile('data.json', JSON.stringify(modified_recipe), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(204).send();
+        }
+      });
+    };
+
+    recipe_exists ? writeFile() : res.status(404).send(error_string);
+  });
 });
